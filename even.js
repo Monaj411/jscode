ccdd1c570fbdd05f1066d887c3e92f92e7a79e5b
Ahbideen Yusuf <ahbideenyusuf@gmail.com>
2023-11-19 11:06:03 +0100

    Review even

--- even.js
+++ even.js
@@ -1,11 +1,18 @@
-// In this kata we are passing a number (n) into a function. Your code will determine if the number passed is even (or not). The function needs to return either a true or false. Numbers may be positive or negative, intergers or floats. Floats with decimal part non equal to zero are considered UNeven for this kata.
+// In this kata we are passing a number (n) into a function. Your code will determine if the number passed is even (or not).
+// The function needs to return either a true or false. Numbers may be positive or negative, intergers or floats.
+// Floats with decimal part non equal to zero are considered UNeven for this kata.
 
+// We want to check if the number is even or not
+// The return value is a boolean
+// The floats that has a decimal part that are not zero are considered uneven
 
+// Reviewed
 function testEven(n) {
-    if (Number.isInteger(n)  && n % 2 == 0) {
-        return true;}  
-else {return false;
-}
+  if (Number.isInteger(n) && n % 2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-console.log(testEven(4))
+console.log(testEven(1));
