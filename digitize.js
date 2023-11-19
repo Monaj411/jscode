0fb93b06eaccb1b2247f80291c0125c3efbbd9f1
Ahbideen Yusuf <ahbideenyusuf@gmail.com>
2023-11-19 10:55:26 +0100

    Review digitize

--- digitize.js
+++ digitize.js
@@ -1,6 +1,8 @@
-// Given a random non-negative number, you have to return the digit of this number within an array in reverse ondragover.
+// Given a random non-negative number, you have to return the digit of this number within an array in reverse order.
+
+// Reviewed
 function digitize(n) {
-    const digitize = n.reverse(); 
-    return digitize
-  }
-  console.log(digitize([9, 8, 7, 6, 5, 4, 3, 2, 1]))
+  const digitize = n.reverse();
+  return digitize;
+}
+console.log(digitize([9, 8, 7, 6, 5, 4, 3, 2, 1]));
